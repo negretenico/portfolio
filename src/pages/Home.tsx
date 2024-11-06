08ae5308457bcb239038e8acdba0bfc4fcd5d83f
negretenico <negretenico0@gmail.com>
2024-11-05 20:31:19 -0500

    Adding the final stuff for the home page; need to add new routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,49 @@
 import React from "react";
 import { useCopy } from "../utils/useCopy";
 import { COLORS } from "../constants";
-
+import { useNavigate } from "react-router-dom";
 export default function Home() {
   const { data: copy, isLoading } = useCopy({ filePath: "/copy/home.json" });
+  // const navigate = useNavigate();
   if (isLoading) {
     return <p>Spinner</p>;
   }
   const [beforeComma, afterComma] = copy.title.split(",");
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen">
-        {/* TODO: add svg for our logo*/}
-        <span className="text-5xl mb-5" style={{ color: COLORS.BLACK }}>
-          {beforeComma},
-          <span style={{ color: COLORS.LIGHT_VIOLET }} className="font-bold">
-            {afterComma}
-          </span>
-        </span>
-        <div className="max-w-lg mx-auto">
-          <p className="text-lg text-wrap text-center truncate">
-            {copy.subtitle}
-          </p>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col">
+          <img src="logo.png" alt="logo" className="w-64 h-64 rounded-full" />
         </div>
-        {/* TODO: add clickable link to take us to the contact you page */}
-        <span
-          style={{ color: COLORS.LIGHT_VIOLET }}
-          className="mx-1 font-extrabold relative inline-block stroke-current mt-32"
-        >
-          {copy.footer}
-          <svg
-            className="absolute -bottom-0.5 w-full max-h-1.5"
-            viewBox="0 0 55 5"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
+        <div className="mt-8">
+          <span className="text-6xl mb-5" style={{ color: COLORS.BLACK }}>
+            {beforeComma},
+            <span style={{ color: COLORS.LIGHT_VIOLET }} className="font-bold">
+              {afterComma}
+            </span>
+          </span>
+          <div className="max-w-lg mx-auto mt-4 text-xl">
+            <p className="text-lg text-wrap text-center">{copy.subtitle}</p>
+          </div>
+          <span
+            style={{ color: COLORS.LIGHT_VIOLET, cursor: "pointer" }}
+            // onClick={() => navigate("contact")}
+            className="mx-1 text-lg font-extrabold relative inline-block stroke-current mt-32"
           >
-            <path
-              d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
-              strokeWidth="2"
-            ></path>
-          </svg>
-        </span>
+            {copy.footer}
+            <svg
+              className="absolute -bottom-0.5 w-full max-h-1.5"
+              viewBox="0 0 55 5"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                strokeWidth="2"
+              ></path>
+            </svg>
+          </span>
+        </div>
       </div>
     </>
   );
