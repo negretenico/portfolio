e338bca9a00438f55e84840f7bbc9a9f19b69545
Nico <negretenico@gmail.com>
2024-11-12 17:37:20 -0500

    staring poitn

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,55 +1,40 @@
-import React, { useState } from 'react'
-import { COLORS } from '../constants'
+import React, { useState } from "react";
+import { COLORS } from "../constants";
+import { Button } from "./Button";
 
-const TabContent = ({content}:{content:JSX.Element})=>{
-    return <>
-        <div className="flex-1 p-4">
-          {content}
-        </div>
-        </>
-}
-const Button = ({ onClick,key,isActive,label}:{onClick: ()=>void,key:string,isActive:boolean,label:string})=>{
-    const [isHovered, setIsHovered] = useState(false);
+const TabContent = ({ content }: { content: JSX.Element }) => {
+  return (
+    <>
+      <div className="flex-1 p-4">{content}</div>
+    </>
+  );
+};
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
-  
-    const className = `py-2 px-4 text-left rounded-lg w-full mb-2 transition-colors`;
-  
-    // Set the background and text color based on the state
-    const backgroundColor = isActive
-      ? COLORS.VIOLET
-      : isHovered
-      ? COLORS.LIGHT_VIOLET // Add hover color
-      : COLORS.WHITE;
-  
-    const textColor = (isActive || isHovered) ? COLORS.WHITE : COLORS.BLACK; 
-       return (<button style={{
-        backgroundColor,
-        color: textColor
-    }} key={key} onClick={onClick}  onMouseEnter={handleMouseEnter} // Trigger hover effect
-    onMouseLeave={handleMouseLeave}  className={className}>{label}</button>)
-}
+export const Tabs = ({
+  tabs,
+  isVertical,
+}: {
+  tabs: Array<{ id: number; label: string; content: JSX.Element }>;
+  isVertical?: boolean;
+}) => {
+  const [activeTab, setActiveTab] = useState(0); // Initially, the first tab is active
 
-export const Tabs = ({tabs,isVertical}:{tabs:Array<{id:number,label:string,content:JSX.Element}>,isVertical:boolean})=>{
-    const [activeTab, setActiveTab] = useState(0); // Initially, the first tab is active
-  
-    return (
-      <div className="flex">
-        <div style={{backgroundColor:COLORS.WHITE}} className={`flex flex-${isVertical?'col':'row'} w-48 p-2 border-r`}>
-          {tabs.map((tab) => (
-            <Button isActive={activeTab === tab.id}
-              key={`${tab.id}`}
-              onClick={() => setActiveTab(tab.id)}
-              label={tab.label}/>
-          ))}
-        </div>
-        <TabContent content={tabs[activeTab].content} />
+  return (
+    <div className="flex">
+      <div
+        style={{ backgroundColor: COLORS.WHITE }}
+        className={`flex flex-${isVertical ? "col" : "row"} w-48 p-2 border-r`}
+      >
+        {tabs.map((tab) => (
+          <Button
+            isActive={activeTab === tab.id}
+            key={`${tab.id}`}
+            onClick={() => setActiveTab(tab.id)}
+            label={tab.label}
+          />
+        ))}
       </div>
-    )
-}
+      <TabContent content={tabs[activeTab].content} />
+    </div>
+  );
+};
--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { useCopy } from '../../utils/useCopy'
-import { Text } from '../../components/Text'
-import { Tabs } from '../../components/Tabs'
-import Accordion from '../../components/Accordion'
+import React from "react";
+import { useCopy } from "../../utils/useCopy";
+import { Text } from "../../components/Text";
+import { Tabs } from "../../components/Tabs";
+import Accordion from "../../components/Accordion";
 
-//TODO: add the following, FRONTEND: Clean plate club, Micro frontends, porfolio, anime website 
-//TODO: add the follwoing, BACKEND: CLean plate club, Ticketmaster, Service Sentinel, 
+//TODO: add the following, FRONTEND: Clean plate club, Micro frontends, porfolio, anime website
+//TODO: add the follwoing, BACKEND: CLean plate club, Ticketmaster, Service Sentinel,
 //TODO: add the folowing, AI: NBA clustering, Room Recognition, Champion Classifier, Sarcasm
 //TODO: add teh following, MISC: Algorithims, Path visualization, 2D Game Remake
 
-
 export default function Gallery() {
-    const {data:copy}  = useCopy({filePath:'/copy/gallery.json'})
-      
-    const tabs = [
-        { id: 0, label: "Tab 1", content: <Accordion/>},
-        { id: 1, label: "Tab 2", content: <>Hi2</> },
-        { id: 2, label: "Tab 3", content: <>Hi3</> },
-        { id: 3, label: "Tab 4", content: <>Hi4</>},
-      ];
+  const { data: copy } = useCopy({ filePath: "/copy/gallery.json" });
+
+  const tabs = [
+    { id: 0, label: "Tab 1", content: <Accordion /> },
+    { id: 1, label: "Tab 2", content: <>Hi2</> },
+    { id: 2, label: "Tab 3", content: <>Hi3</> },
+    { id: 3, label: "Tab 4", content: <>Hi4</> },
+  ];
   return (
     <>
-    <div>
+      <div>
         <Text text={copy?.title} />
-        <Text text={copy?.subtitle}/>
-        <div className='max-w-lg'> 
-        <Tabs isVertical tabs={tabs}/>
+        <Text text={copy?.subtitle} />
+        <div className="max-w-lg">
+          <Tabs isVertical tabs={tabs} />
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
