f14ef60b64a503c2c8c25356a15762a2da9729e5
negretenico <negretenico0@gmail.com>
2024-11-12 20:08:03 -0500

    Addig the mdb5 tool kit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import App from "./App";
 // @ts-ignore
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 // @ts-ignore
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
